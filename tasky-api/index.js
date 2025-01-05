66ac0b5e9f456d7ed34afe9ea083fa13c9114208
Nova Vina <20081125@mail.wit.ie>
2025-01-05 21:22:29 +0000

    Mongo lab Integrated Tasky React App

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
 import usersRouter from './api/users';
+import cors from 'cors';
 
 
 
@@ -20,6 +21,8 @@ const errHandler = (err, req, res, next) => {
 const app = express();
 
 const port = process.env.PORT;
+
+app.use(cors());
 
 app.use(express.json());
 
