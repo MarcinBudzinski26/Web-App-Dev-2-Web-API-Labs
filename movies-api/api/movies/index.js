0d24755c7acbf018a74bb2a4f6032d87209881a7
Nova Vina <20081125@mail.wit.ie>
2025-01-06 14:52:26 +0000

    Authentication lab - exercise (genres)

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -2,7 +2,8 @@ import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
 import {
-    getUpcomingMovies
+    getUpcomingMovies,
+    getMovieGenres
   } from '../tmdb-api';
   
 const router = express.Router();
@@ -44,6 +45,12 @@ router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
     const upcomingMovies = await getUpcomingMovies();
     res.status(200).json(upcomingMovies);
 }));
+
+router.get('/tmdb/genres', asyncHandler(async (req, res) => {
+    const genres = await getMovieGenres();
+    res.status(200).json(genres);
+}));
+
 
 
 export default router;
